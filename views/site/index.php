093c2405dac08c98e0394038c3d2caaa71cca62d
Lucas Maués <lucasmaues@lucasmaues.com>
2019-06-11 23:41:43 -0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Gerador';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Gerador</h1>
+
+        <p class="lead">This is an Yii application to use Gii for generate codes.</p>
+
+        <p><a class="btn btn-lg btn-success" href="gii">Start Gii</a></p>
+    </div>
+
+</div>
